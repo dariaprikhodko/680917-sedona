a39dfdd9f495a2fdaecd9e2a880c04506c538425
dariaprikhodko <favour313@mail.ru>
2018-11-10 02:46:24 +1100

    поправка

--- gulpfile.js
+++ gulpfile.js
@@ -102,5 +102,4 @@ gulp.task("clean", function () {
 
 
 gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
-gulp.task("start", gulp.series("css", "server"));
 gulp.task("start", gulp.series("build", "server"));
